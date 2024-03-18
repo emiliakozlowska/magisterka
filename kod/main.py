6eef7af9e6f04b78bb8c59af4ebe9f7cb94b937c
Emilia Kozłowska <115942148+emiliakozlowska@users.noreply.github.com>
2024-03-18 21:49:31 +0100

    Add files via upload

--- kod/main.py
+++ kod/main.py
@@ -2,10 +2,23 @@ import pandas as pd
 import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
+import random as rn
+import xgboost as xgb
+import tensorflow as tf
+
 from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split, GridSearchCV
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+from scikeras.wrappers import KerasClassifier
 
 
 risk_df = pd.read_csv(r"D:\Studia\0. SGH\MAGISTERKA\dane\Maternal Health Risk Data Set.csv", sep=';', decimal=',')
@@ -14,7 +27,7 @@ risk_df['RiskLevel'].unique()
 risk_df.isnull().sum()
 risk_df.info()
 
-mapping = {'low risk': 1, 'mid risk': 2, 'high risk': 3}
+mapping = {'low risk': 0, 'mid risk': 1, 'high risk': 2}
 risk_df['RiskLevel'] = risk_df['RiskLevel'].map(mapping)
 risk_df[['RiskLevel']].value_counts()
 
@@ -83,19 +96,19 @@ risk_df_filtered
 
 ###############################################################################
 
-# accuracy, precision, recall, and F1 score
-
-# Drzewa decyzyjne
+# DRZEWA DECYZYJNE
 
 X = risk_df_filtered[X_col_names]
 y = risk_df_filtered['RiskLevel']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
 # Definiowanie siatki parametrów do przeszukiwania
 param_grid = {
-    'max_depth': [None, 5, 10, 15, 20, 25],
-    'min_samples_split': [2, 5, 10, 15, 20],
-    'min_samples_leaf': [1, 2, 4, 6, 8]
+    'max_depth': [5, 10, 15,],
+    'min_samples_split': [2, 5, 10, 15],
+    'min_samples_leaf': [2, 4, 5],
+    'criterion': ['gini', 'entropy'],
+    'splitter': ['best', 'random']
 }
 
 # Przeszukiwanie siatki w celu znalezienia najlepszego zestawu parametrów
@@ -129,9 +142,20 @@ final_clf.fit(X_train, y_train)
 # Ocena modelu
 y_pred = final_clf.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
+conf_matrix = confusion_matrix(y_test, y_pred)
 print(f'Dokładność modelu: {accuracy:.2f}')
 print(classification_report(y_test, y_pred))
+print(conf_matrix)
 
+# Wykres macieży pomyłek
+labels = ['Low Risk', 'Medium Risk', 'High Risk']
+plt.figure(figsize=(10, 7))
+sns.heatmap(conf_matrix, annot=True, fmt='g', cmap='Blues', xticklabels=labels, yticklabels=labels)
+plt.title('Macierz pomyłek')
+plt.xlabel('Przewidywane etykiety')
+plt.ylabel('Prawdziwe etykiety')
+plt.show()
+ 
 # Wizualizacja ważności cech
 feature_importances = final_clf.feature_importances_
 features = X.columns
@@ -155,60 +179,212 @@ plot_tree(final_clf,
           impurity=True)
 plt.show()
 
-###############################################################################
+######################
+# ROC? można dla każdej klasy osobno zrobić
 
-# Lasy losowe
-from sklearn.ensemble import RandomForestClassifier
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-random_forest_model = RandomForestClassifier(n_estimators=100, random_state=42)
-random_forest_model.fit(X_train, y_train)
-y_pred = random_forest_model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Dokładność modelu: {accuracy:.2f}')
-print(classification_report(y_test, y_pred))
 
 ###############################################################################
 
-# Gradient Boosting
-from sklearn.ensemble import GradientBoostingClassifier
+# LASY LOSOWE
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-gradient_boosting_model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
-gradient_boosting_model.fit(X_train, y_train)
-y_pred = gradient_boosting_model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Dokładność modelu: {accuracy:.2f}')
-print(classification_report(y_test, y_pred))
+
+# Podstawowy model z 100 drzewami
+random_forest_100 = RandomForestClassifier(n_estimators = 100, 
+                                           criterion = 'gini',
+                                           max_depth = None,
+                                           min_samples_leaf = 1,
+                                           min_weight_fraction_leaf = 0.0,
+                                           max_features = 'sqrt',
+                                           max_leaf_nodes = None,
+                                           bootstrap = True,
+                                           oob_score = False,
+                                           min_impurity_decrease = 0.0,                                           
+                                           random_state = 1)
+random_forest_100.fit(X_train, y_train)
+
+# Ocena modelu na zbiorze treningowym i testowym
+y_pred_train = random_forest_100.predict(X_train)
+accuracy_train = accuracy_score(y_train, y_pred_train)
+y_pred_test = random_forest_100.predict(X_test)
+accuracy_test = accuracy_score(y_test, y_pred_test)
+print("Dokładność predykcji na zbiorze treningowym:", accuracy_train)
+print("Dokładność predykcji na zbiorze testowym:", accuracy_test)
+print(classification_report(y_test,random_forest_100.predict(X_test)))
+conf_matrix = confusion_matrix(y_test, y_pred)
+print(conf_matrix)
+
+# Szukanie hiperparametrów
+random_forest = RandomForestClassifier(random_state=42)
+param_grid = {
+    'n_estimators': [50, 100, 150],
+    'max_depth': [10, 20, 30],
+    'min_samples_leaf': [1, 2, 3],
+    'criterion': ['gini', 'entropy'],
+    'min_samples_split': [2, 3, 4]
+}
+grid_search = GridSearchCV(estimator=random_forest, param_grid=param_grid, cv=5, n_jobs=-1, verbose=2, scoring='accuracy')
+grid_search.fit(X_train, y_train)
+print("Najlepsze parametry:", grid_search.best_params_)
+print("Najlepsza dokładność:", grid_search.best_score_)
+best_model = grid_search.best_estimator_
+
+# Ocena modelu na zbiorze treningowym i testowym
+y_pred_train = best_model.predict(X_train)
+accuracy_train = accuracy_score(y_train, y_pred_train)
+y_pred_test = best_model.predict(X_test)
+accuracy_test = accuracy_score(y_test, y_pred_test)
+print("Dokładność predykcji na zbiorze treningowym:", accuracy_train)
+print("Dokładność predykcji na zbiorze testowym:", accuracy_test)
+print(classification_report(y_test, best_model.predict(X_test)))
+conf_matrix = confusion_matrix(y_test, y_pred)
+print(conf_matrix)
+
+# Ważnoć cech
+feature_importance = best_model.feature_importances_
+feature_score = pd.Series(feature_importance, index=X_train.columns)
+score = feature_score.sort_values()
+plt.figure(figsize=(12,5))
+plt.barh(y=score.index, width=score.values, color='blue')
+plt.grid(alpha=0.4)
+plt.title('Feature Importance of Best Model')
+plt.show()
 
 ###############################################################################
 
-# SVM
-from sklearn.svm import SVC
+# XGBoost
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-svm_model = SVC(kernel='rbf', C=1.0, gamma='scale', random_state=42)
+xgb_model = xgb.XGBClassifier(objective='multi:softmax', num_class=3, random_state=42)
+xgb_model.fit(X_train, y_train)
+y_pred_xgb = xgb_model.predict(X_test)
+
+# Ocena modelu
+accuracy = accuracy_score(y_test, y_pred_xgb)
+print("Dokładność predykcji: %.2f%%" % (accuracy * 100.0))
+y_pred_train = xgb_model.predict(X_train)
+accuracy_train = accuracy_score(y_train, y_pred_train)
+y_pred_test = xgb_model.predict(X_test)
+accuracy_test = accuracy_score(y_test, y_pred_test)
+print("Dokładność predykcji na zbiorze treningowym:", accuracy_train)
+print("Dokładność predykcji na zbiorze testowym:", accuracy_test)
+print(classification_report(y_test, y_pred_test))
+conf_matrix = confusion_matrix(y_test, y_pred_test)
+print(conf_matrix)
+
+# Poszukiwanie hiperparametrów
+param_grid = {
+    'n_estimators': [100, 200, 300],
+    'learning_rate': [0.1, 0.2, 0.3],
+    'max_depth': [10, 20, 50],
+    'min_child_weight': [2, 3, 4],
+    'gamma': [0, 0.1, 0.2],
+    'reg_alpha': [0, 0.001, 0.01],
+    'reg_lambda': [0, 0.001, 0.01]
+}
+grid_search = GridSearchCV(estimator=xgb_model, param_grid=param_grid, cv=3, scoring='accuracy', verbose=2, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+print("Najlepsze parametry:", grid_search.best_params_)
+print("Najlepsza dokładność:", grid_search.best_score_)
+best_model = grid_search.best_estimator_
+y_pred_test = best_model.predict(X_test)
+
+# Ocena modelu
+accuracy = accuracy_score(y_test, y_pred_test)
+print("Dokładność predykcji: %.2f%%" % (accuracy * 100.0))
+y_pred_train = best_model.predict(X_train)
+accuracy_train = accuracy_score(y_train, y_pred_train)
+accuracy_test = accuracy_score(y_test, y_pred_test)
+print("Dokładność predykcji na zbiorze treningowym:", accuracy_train)
+print("Dokładność predykcji na zbiorze testowym:", accuracy_test)
+print(classification_report(y_test, y_pred_test))
+conf_matrix = confusion_matrix(y_test, y_pred_test)
+print(conf_matrix)
+
+# Ważnoć cech
+feature_importance = best_model.feature_importances_
+feature_score = pd.Series(feature_importance, index=X_train.columns)
+score = feature_score.sort_values()
+plt.figure(figsize=(12,5))
+plt.barh(y=score.index, width=score.values, color='blue')
+plt.grid(alpha=0.4)
+plt.title('Feature Importance of Best Model')
+plt.show()
+
+###############################################################################
+
+# SVM (do wywalenia)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+svm_model = SVC(kernel='rbf', probability=True, random_state=42)
 svm_model.fit(X_train, y_train)
 y_pred = svm_model.predict(X_test)
+
+# Ocena modelu
 accuracy = accuracy_score(y_test, y_pred)
-print(f'Dokładność modelu: {accuracy:.2f}')
+print("Dokładność predykcji: %.2f%%" % (accuracy * 100.0))
 print(classification_report(y_test, y_pred))
+conf_matrix = confusion_matrix(y_test, y_pred)
+print(conf_matrix)
 
-###############################################################################
-
-# SGD
-from sklearn.linear_model import SGDClassifier
-
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-sgd_model = SGDClassifier(loss='hinge', penalty='l2', alpha=0.0001, max_iter=1000, tol=1e-3, random_state=42)
-sgd_model.fit(X_train, y_train)
-y_pred = sgd_model.predict(X_test)
-accuracy = accuracy_score(y_test, y_pred)
-print(f'Dokładność modelu: {accuracy:.2f}')
-print(classification_report(y_test, y_pred))
+# Szukanie hiperparametów
+param_grid = {
+    'C': [0.1, 1, 10],  # Parametr regularyzacji
+    'gamma': ['scale', 0.001, 0.01, 0.1],
+}
+grid_search = GridSearchCV(estimator=svm_model, param_grid=param_grid, cv=3, scoring='accuracy', verbose=2, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+print("Najlepsze parametry:", grid_search.best_params_)
+print("Najlepsza dokładność:", grid_search.best_score_)
 
 ###############################################################################
 
 # Sieci neuronowe
+
+risk_df_sieci.head()
+X = risk_df_sieci[X_col_names]
+y = risk_df_sieci['RiskLevel']
+y_one_hot = to_categorical(y)
+X_train, X_test, y_train, y_test = train_test_split(X, y_one_hot, test_size=0.3, random_state=42)
+
+# Przekształcenie zmiennych
+num_transformer = StandardScaler()
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', num_transformer, X_col_names)
+    ]) 
+X_train = preprocessor.fit_transform(X_train)
+X_test = preprocessor.transform(X_test)
+
+np.random.seed(42)
+rn.seed(42)
+tf.random.set_seed(42)
+
+# Sieć 1
+siec1 = Sequential()
+siec1.add(Dense(6, input_dim=X_train.shape[1], activation='relu'))
+siec1.add(Dense(3, activation='softmax'))
+siec1.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+siec1.fit(X_train, y_train, epochs=50, batch_size=10)
+
+accuracy_train = siec1.evaluate(X_train, y_train)
+print('Accuracy: %.2f' % (accuracy_train[1]*100))
+accuracy_test = siec1.evaluate(X_test, y_test)
+print('Accuracy: %.2f' % (accuracy_test[1]*100))
+
+# Sieć 2 - wybór liczby epok
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, verbose=1)
+siec2t = Sequential()
+siec2t.add(Dense(6, input_dim=X_train.shape[1], activation='relu'))
+siec2t.add(Dense(3, activation='softmax'))
+siec2t.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+siec2t.fit(X_train, y_train, epochs=200, batch_size=10, 
+           validation_data=(X_test, y_test), 
+           callbacks=[early_stopping])
+
+
+
+
 
 
 
